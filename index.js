d4035b352325be0835a27281c161353b4ea01af0
Nosha08 <noah.rozin@outlook.com>
2023-10-18 21:59:53 -0400

    a

--- index.js
+++ index.js
@@ -1,10 +1,44 @@
 const DOMSelectors = {
-    form: document.querySelector('#form'),
-    imagPers: document.querySelector('.imagePers'),
-    h2s: document.querySelectorAll('h2')
+    form: document.querySelector('.form'),
+    name: document.querySelector('#name'),
+    img: document.querySelector('#img'),
+    desc: document.querySelector('#desc')
+}
+
+const HTMLSelectors = {
+    container: document.querySelector('.container'),
+    HTMLname: document.querySelector('#person-name'),
+    HTMLimg: document.querySelector('#person-img'),
+    HTMLdesc: document.querySelector('#person-desc'),
 }
 
 DOMSelectors.form.addEventListener('submit', function (event) {
-    event.preventDefault();
-    DOMSelectors.h2s.forEach((header) => header.textContent = DOMSelectors.firstname.value)
-});
+    event.preventDefault()
+    create()
+})
+
+function create() {
+    const person = {
+        name: DOMSelectors.name.value,
+        img: DOMSelectors.img.value,
+        desc: DOMSelectors.desc.value
+    }
+
+    inject(person)
+    clear()
+    remove()
+}
+
+function inject(person) {
+    HTMLSelectors.HTMLname.innerHTML = person.name
+    HTMLSelectors.HTMLimg.innerHTML = person.img
+    HTMLSelectors.HTMLdesc.innerHTML = person.desc
+}
+
+function clear() {
+    
+}
+
+function remove() {
+
+}
